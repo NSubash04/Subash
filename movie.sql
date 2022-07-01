6f9d3cd797b23457f6379e7c860dced3bbf67bb4
NSubash04 <108538330+NSubash04@users.noreply.github.com>
2022-07-01 20:51:34 +0530

    Add files via upload

---
+++ movie.sql
@@ -0,0 +1,5 @@
+create table movie(movie_name varchar(20),lead_actor_name varchar(20),actress_name varchar(20),year_of_releas INTEGER,director_name varchar(20))
+insert into movie values("Bigil","Vijay","Nayanthara",2019,"Atlee");
+insert into movie values("Master","Vijay","MalavikaMohan",2021,"Lokesh");
+insert into movie values("Sarkar","Vijay","Keerthisuresh",2018,"Murugadoss");
+insert into movie values("Karnan","Dhanush","RajishaVijayam",2020,"MariSelvaraj");
